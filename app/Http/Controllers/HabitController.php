e3adc3ddb69169d4fb3b2adcd0e255d8a17107c7
João Fernandes <joaofernandes@cs-consoft.com.br>
2026-05-17 19:19:41 -0300

    cria a feat de criar novos habitos

---
+++ app/Http/Controllers/HabitController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Habit;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\HabitRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class HabitController extends Controller
+{
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('habit.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(HabitRequest $request)
+    {
+        $validated = $request->validated();
+
+        $request->user()->habits()->create($validated);
+
+        return redirect()
+            ->route('site.dashboard')
+            ->with('success', 'Hábito criado com sucesso!');
+    }
+
+
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Habit $habit)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Habit $habit)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Habit $habit)
+    {
+        //
+    }
+}
